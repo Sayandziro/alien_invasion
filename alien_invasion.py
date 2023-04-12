61d3e602ad1886a7dcf9aca1510de8ac6aa18202
sayandziro <sayan@gmail.com>
2023-04-12 17:43:02 +0300

    Connected to github

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,7 +11,6 @@ class AlienInvasion:
 		"""Initialize the game, and create game resources."""
 		pygame.init()
 		self.settings = Settings()
-
 		self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
 		pygame.display.set_caption("Alien Invasion")
 
@@ -30,17 +29,27 @@ class AlienInvasion:
 			if event.type == pygame.QUIT:
 				sys.exit()
 			elif event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_RIGHT:
-					# Move the ship to the right.
-					self.ship.moving_right = True
-				elif event.key == pygame.K_LEFT:
-					# Move the ship to the left.
-					self.ship.moving_left = True
+				self._check_keydownn_events(event)
 
 			elif event.type == pygame.KEYUP:
-				if event.key == pygame.K_RIGHT:
+				self._check_keyup_events(event)
+
+
+	def _check_keydownn_events(self, event):
+		if event.key == pygame.K_RIGHT:
+			# Move the ship to the right.
+			self.ship.moving_right = True
+		elif event.key == pygame.K_LEFT:
+			# Move the ship to the left.
+			self.ship.moving_left = True
+		if event.key == pygame.K_q:
+			sys.exit()
+
+
+	def _check_keyup_events(self, event):
+		if event.key == pygame.K_RIGHT:
 					self.ship.moving_right = False
-				elif event.key == pygame.K_LEFT:
+		elif event.key == pygame.K_LEFT:
 					self.ship.moving_left = False
 
 
